e72f36a78137edc6cab4e86a972be4f807e18af5
erkin7333 <erkinnajimov90@gmail.com>
2022-11-29 12:19:44 +0500

    Cart peji o'zgardi

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.files import File
+from io import BytesIO
+from PIL import Image
 
 class Category(models.Model):
     title = models.CharField(max_length=50)
@@ -30,7 +32,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=50)
     description = models.TextField(blank=True)
     price = models.FloatField()
-    image = models.ImageField(upload_to='product')
+    image = models.ImageField(upload_to='product', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='product/thumbnail', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -40,6 +42,30 @@ class Product(models.Model):
     def get_diplay_price(self):
         return self.price / 100
 
+    def make_thumbnail(self, image, size=(1000, 1000)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+        name = image.name.replace('uploads/product_images/', '')
+        thumbnail = File(thumb_io, name=name)
+        return thumbnail
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                return self.thumbnail.url
+            else:
+                return 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fnews%2Fpython-crash-course%2F&psig=AOvVaw0YGRHVX6I6UVvgOuqmtYom&ust=1669789846547000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPDm1Ybi0vsCFQAAAAAdAAAAABAE'
+
     class Meta:
         verbose_name = "Maxsulot"
         ordering = ('-created_at',)
+
+
